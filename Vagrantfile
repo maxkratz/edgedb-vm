289e509d14e2f992f84c40ff15688af8781c4005
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-08-15 09:32:55 +0200

    Adds the first vagrant config + CI draft

---
+++ Vagrantfile
@@ -0,0 +1,15 @@
+token = ENV["GITHUB_TOKEN"]
+
+Vagrant.configure("2") do |config|
+    config.vm.box = "gusztavvargadr/ubuntu-desktop-2004-lts-xfce"
+    config.vm.define 'edgedb'
+    config.vm.provider :virtualbox do |vb|
+        vb.name = "edgedb"
+        vb.memory = 8192
+        vb.cpus = 2
+    end
+    config.vm.provision "shell", path: "prov.sh", env: {"GITHUB_TOKEN" => token}, privileged: false
+
+    # Prevent SharedFoldersEnableSymlinksCreate errors
+    config.vm.synced_folder ".", "/vagrant", disabled: true
+end
---
+++ prov.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+# Config
+#
+
+set -e
+START_PWD=$PWD
+
+#
+# Utils
+#
+
+# Displays the given input including "=> " on the console.
+log () {
+	echo "=> $1"
+}
+
+#
+# Script
+#
+
+log "Start provisioning."
+
+# Updates
+log "Installing updates."
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Packages for building a new kernel
+sudo apt-get install -y gcc make perl curl
+
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.edgedb.com | sh
+
+log "Clean-up"
+sudo apt-get remove -yq \
+        snapd \
+        libreoffice-* \
+        thunderbird \
+        pidgin \
+        gimp \
+        evolution
+sudo apt-get autoremove -yq
+sudo apt-get clean cache
+
+log "Finished provisioning."
